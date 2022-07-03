4a06a6c38cd84b0689cb841b62c4b0bf8c7cf9f7
Happy Gilmore <maryleone@rocketcentral.com>
2022-07-03 13:21:49 -0400

    created tables

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,8 +9,44 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull:false,
+      primaryKey:true,
+      autoIncrement:true
   },
+
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull:false,
+    },
+
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull:false,
+      validate: {
+        isDecimal: true
+      }
+    },
+
+      stock:{
+        type: DataTypes.INTEGER,
+        allowNull:false,
+        validate: {
+          isNumeric: true
+        }
+        //Set a default value of 10
+      },
+
+      category_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model:'category',
+          key:'id'
+        }
+      }
+    },
+
   {
     sequelize,
     timestamps: false,
